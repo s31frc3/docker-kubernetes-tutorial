524b2c07a721594bb192b7ee74bfb98612473db4
Andrei <zenin.an@do-student.ru>
2023-04-28 12:49:46 +0300

    some changes

---
+++ feb_calculator/server/index.js
@@ -0,0 +1,5 @@
+const keys = require('./keys');
+
+//express app setup
+const express = require('express');
+const bodyParser = require('body-parser')
---
+++ feb_calculator/server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    residPort: process.env.REDIS_PORT,
+    gpUser: process.env.PGUSER,
+    gpHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    psPort: process.env.PGPORT
+};
