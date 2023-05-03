49d34fd03e35fc29fbb4232dc88b2de9730babd3
Andrei <zenin.an@do-student.ru>
2023-05-03 06:47:39 +0300

    fixed typos

--- feb_calculator/server/keys.js
+++ feb_calculator/server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    residPort: process.env.REDIS_PORT,
-    gpUser: process.env.PGUSER,
-    gpHost: process.env.PGHOST,
-    pgDatabase: process.env.PGDATABASE,
-    pgPassword: process.env.PGPASSWORD,
-    pgPort: process.env.PGPORT
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
 };
